17e9a6c76bec3f070ba9241f39fba36f9c3d9ed0
Rory Murray <rory.christian.murray@gmail.com>
2019-08-05 17:35:53 -0400

    MVP commit!

--- src/snackApp/Main.java
+++ src/snackApp/Main.java
@@ -24,17 +24,17 @@ public class Main {
         Snack snack5 = new Snack("Water", 20, 2.75, vendy2.getId());
 
         // Begin actions
-        System.out.println("Snack Bar line begins here")
+        System.out.println("Snack Bar line begins here");
 
         // Customer 1 buys 3 of snack 4
         custy1.makePurchase(snack4.totalCost(3));
         snack4.buySnack(3);
 
         // Print Customer 1 cash on hand
-        System.out.println("Customer 1 has " + custy1.getCash() " cash on hand");
+        System.out.println("Customer 1 has " + custy1.getCash() + " cash on hand");
 
         // Print quantity of snack 4
-        System.out.println("There are " + snack4.getQuantity() " amount of Snack 4 left");
+        System.out.println("There are " + snack4.getQuantity() + " amount of Snack 4 left");
 
         // Customer 1 buys 1 of snack 3
         custy1.makePurchase(snack3.totalCost(1));
@@ -51,24 +51,23 @@ public class Main {
         snack4.buySnack(2);
 
         // Print Customer 2 cash on hand
-        System.out.println("Customer 2 has " + custy2.getCash() " cash on hand");
+        System.out.println("Customer 2 has " + custy2.getCash() + " cash on hand");
 
         // Print quantity of snack 4
-        System.out.println("There are " + snack4.getQuantity() " of Snack 4 left");
-
+        System.out.println("There are " + snack4.getQuantity() + " of Snack 4 left");
 
         // Customer 1 finds $10
         custy1.addCash(10);
 
         // Print Customer 1 cash on hand
-        System.out.println("Customer 1 has " custy1.getCash() " amount of cash on hand");
+        System.out.println("Customer 1 has " + custy1.getCash() + " amount of cash on hand");
 
         // Customer 1 buys 1 of snack 2
         custy1.makePurchase(snack2.totalCost(1));
         snack2.buySnack(1);
 
         // Print Customer 1 cash on hand
-        System.out.println("Customer 1 has " custy1.getCash() + " amount of cash on hand");
+        System.out.println("Customer 1 has " + custy1.getCash() + " amount of cash on hand");
 
         // Print quantity of snack 2
         System.out.println("There are " + snack2.getQuantity() + " amount of Snack 2 left");
